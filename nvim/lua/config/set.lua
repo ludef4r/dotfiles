a9fbf7e68532ecb91c446ea63b7d7bc9562ca402
ludef4r <generic.email.for.coding.projects@gmail.com>
2026-07-29 22:20:40 +0000

    feat: Added more options

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -1,9 +1,27 @@
 vim.opt.guicursor = ""
 
-vim.opt.nu = true
+vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.clipboard = "unnamedplus"
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.undofile = true
+
+vim.opt.wrap = false
+
+vim.opt.scrolloff = 8
+
+vim.opt.signcolumn = "yes"
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 vim.opt.splitright = true
 vim.opt.splitbelow = true

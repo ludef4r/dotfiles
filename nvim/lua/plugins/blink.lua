c9318f7c9656adc9c1aab02c18f7bd6c0ef6192a
ludef4r <generic.email.for.coding.projects@gmail.com>
2026-07-31 14:18:58 +0000

    feat: Blink configuration

---
+++ nvim/lua/plugins/blink.lua
@@ -0,0 +1,23 @@
+return {
+	"saghen/blink.cmp",
+
+	version = '1.*',
+	opts = {
+		keymap = { 
+			preset = 'enter',
+			['<C-e>'] = false,
+			['<C-k>'] = false
+		},
+
+		appearance = { nerd_font_variant = 'normal' },
+
+		completion = { documentation = { auto_show = false } },
+
+		sources = {
+			default = { 'lsp', 'path', 'buffer' },
+		},
+
+		fuzzy = { implementation = "prefer_rust_with_warning" }
+	},
+	opts_extend = { "sources.default" }
+}
---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,8 @@
+return {
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"mason-org/mason.nvim",
+		"mason-org/mason-lspconfig.nvim",
+		"saghen/blink.cmp"
+	}
+}

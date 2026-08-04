bf1077da1070424d21f874ee689657461ca75127
ludef4r <generic.email.for.coding.projects@gmail.com>
2026-08-04 22:11:05 +0000

    feat: Treesitter config

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,21 @@
+return {
+    'nvim-treesitter/nvim-treesitter',
+
+    branch = 'master',
+    build = ':TSUpdate',
+
+    opts = {
+        ensure_installed = { 
+            "c", 
+            "lua" 
+        },
+
+        sync_install = false,
+        auto_install = true,
+
+        highlight = { 
+            enable = true,
+            additional_vim_regex_highlighting = false
+        }
+    }
+}

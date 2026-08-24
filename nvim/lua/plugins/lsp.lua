6170d11f9970521ac5da0583e02ddc9c3a82407c
ludef4r <generic.email.for.coding.projects@gmail.com>
2026-08-24 12:05:36 +0000

    feat: Configured diagnostics and some keybindings for lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,6 +1,54 @@
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = {
-		"saghen/blink.cmp"
-	}
+    "neovim/nvim-lspconfig",
+    config = function()
+        vim.lsp.config("lua_ls", {
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { "vim" },
+                    },
+                },
+            },
+        })
+
+        -- Diagnostics
+        vim.diagnostic.config({
+            signs = {
+                text = {
+                    [vim.diagnostic.severity.ERROR] = "■",
+                    [vim.diagnostic.severity.WARN] = "■",
+                    [vim.diagnostic.severity.INFO] = "",
+                    [vim.diagnostic.severity.HINT] = "■",
+                },
+            },
+            underline = true,
+            update_in_insert = false,
+        })
+
+        local function set_diagnostic_highlights()
+            vim.api.nvim_set_hl(0, "DiagnosticSignError", { fg = "#FF4444" })
+            vim.api.nvim_set_hl(0, "DiagnosticSignWarn",  { fg = "#D9A83E" })
+            vim.api.nvim_set_hl(0, "DiagnosticSignHint",  { fg = "#4FA6C2" })
+        end
+
+        set_diagnostic_highlights()
+
+        vim.api.nvim_create_autocmd("ColorScheme", {
+            callback = set_diagnostic_highlights
+        })
+
+        -- Keymaps
+        vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float)
+        vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition)
+        vim.keymap.set("n", "<leader>qf", function()
+            local qf_winid = vim.fn.getqflist({ winid = 0 }).winid
+
+            if qf_winid > 0 then
+                vim.cmd("cclose")
+            else
+                vim.diagnostic.setqflist()
+                vim.cmd("copen")
+            end
+        end)
+    end
 }

1b5864b9365b47749177e467fcc1c153970ab63b
ludef4r <generic.email.for.coding.projects@gmail.com>
2026-09-12 15:19:23 +0000

    refac: Removed F1 default keybinding in netrw

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local out = vim.fn.system({ 
-        "git", 
-        "clone", 
-        "--filter=blob:none", 
-        "--branch=stable", 
-        "https://github.com/folke/lazy.nvim.git", 
-        lazypath 
+    local out = vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "--branch=stable",
+        "https://github.com/folke/lazy.nvim.git",
+        lazypath
     })
 
     if vim.v.shell_error ~= 0 then
--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -7,3 +7,9 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex);
 -- Move with identation correct
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Disable netrw F1 keymap
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "netrw",
+    callback = function() pcall(vim.keymap.del, "n", "<F1>", { buffer = true }) end,
+})
